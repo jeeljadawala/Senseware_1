89f710bbbf0319dc1b25131dae4cbb80cf94ad04
jeeljadawala <64966081+jeeljadawala@users.noreply.github.com>
2023-03-19 09:21:02 -0400

    UI updates

--- lib/new.dart
+++ lib/new.dart
@@ -1,253 +1,253 @@
-import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
-
-import 'package:riverpod/riverpod.dart';
-import 'package:senseware/shared_pref.dart';
-import 'localization_helper.dart';
-import 'local_provider.dart';
-import 'supported_locale.dart';
-
-
-// void main() async{
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await AppSharedPreference.init();
-//   runApp(ProviderScope(child: LocalizationDemo()));
-//
-// }
-
-class LocalizationDemo extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (BuildContext context,
-          T Function<T>(ProviderBase<Object, T>) watch, Widget child) {
-        final locale = watch(localeProvider);
-        return MaterialApp(
-          title: 'Localization Demo',
-          debugShowCheckedModeBanner: false,
-          theme: ThemeData(
-            primarySwatch: Colors.blue,
-            visualDensity: VisualDensity.adaptivePlatformDensity,
-          ),
-          locale: locale,
-          supportedLocales: const [
-            Locale('en', 'US'),
-            Locale('fr', 'FR'),
-          ],
-          localizationsDelegates: const [
-            GlobalCupertinoLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            LocalizationHelper.delegate
-          ],
-          home: child,
-        );
-      },
-      child: MenuPage(),
-    );
-  }
-}
-
-class MenuPage extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('${LocalizationHelper.of(context)?.appName}'),
-        actions: [
-          PopupMenuButton<SupportedLocale>(
-            itemBuilder: (context) {
-              return SupportedLocale.values
-                  .map<PopupMenuEntry<SupportedLocale>>((e) => PopupMenuItem(
-                child: Text('${e.name}'),
-                value: e,
-              ))
-                  .toList();
-            },
-            onSelected: (locale) {
-              context.read(localeProvider.notifier).changeLanguage(locale);
-            },
-          )
-        ],
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            const Spacer(
-              flex: 3,
-            ),
-            const Spacer(),
-            Text('${LocalizationHelper.of(context)?.description}',
-                textAlign: TextAlign.start,
-                style: Theme.of(context).textTheme.headline5),
-            const Spacer(
-              flex: 3,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-
 // import 'package:flutter/material.dart';
+// import 'package:flutter_localizations/flutter_localizations.dart';
 //
-// void main() {
-//   runApp(const MyApp());
-// }
+// import 'package:riverpod/riverpod.dart';
+// import 'package:senseware/shared_pref.dart';
+// import 'localization_helper.dart';
+// import 'local_provider.dart';
+// import 'supported_locale.dart';
 //
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
 //
-//   // This widget is the root of your application.
+// // void main() async{
+// //   WidgetsFlutterBinding.ensureInitialized();
+// //   await AppSharedPreference.init();
+// //   runApp(ProviderScope(child: LocalizationDemo()));
+// //
+// // }
+//
+// class LocalizationDemo extends StatelessWidget {
+//
 //   @override
 //   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       title: 'Flutter Demo',
-//       theme: ThemeData(
-//         // This is the theme of your application.
-//         //
-//         // Try running your application with "flutter run". You'll see the
-//         // application has a blue toolbar. Then, without quitting the app, try
-//         // changing the primarySwatch below to Colors.green and then invoke
-//         // "hot reload" (press "r" in the console where you ran "flutter run",
-//         // or simply save your changes to "hot reload" in a Flutter IDE).
-//         // Notice that the counter didn't reset back to zero; the application
-//         // is not restarted.
-//         primarySwatch: Colors.blue,
-//       ),
-//       home: const MyMenuPage(title: 'Flutter Demo Home Page'),
+//     return Consumer(
+//       builder: (BuildContext context,
+//           T Function<T>(ProviderBase<Object, T>) watch, Widget child) {
+//         final locale = watch(localeProvider);
+//         return MaterialApp(
+//           title: 'Localization Demo',
+//           debugShowCheckedModeBanner: false,
+//           theme: ThemeData(
+//             primarySwatch: Colors.blue,
+//             visualDensity: VisualDensity.adaptivePlatformDensity,
+//           ),
+//           locale: locale,
+//           supportedLocales: const [
+//             Locale('en', 'US'),
+//             Locale('fr', 'FR'),
+//           ],
+//           localizationsDelegates: const [
+//             GlobalCupertinoLocalizations.delegate,
+//             GlobalMaterialLocalizations.delegate,
+//             GlobalWidgetsLocalizations.delegate,
+//             LocalizationHelper.delegate
+//           ],
+//           home: child,
+//         );
+//       },
+//       child: MenuPage(),
 //     );
 //   }
 // }
 //
-// class MyMenuPage extends StatefulWidget {
-//   const MyMenuPage({super.key, required this.title});
-//
-//   // This widget is the home page of your application. It is stateful, meaning
-//   // that it has a State object (defined below) that contains fields that affect
-//   // how it looks.
-//
-//   // This class is the configuration for the state. It holds the values (in this
-//   // case the title) provided by the parent (in this case the App widget) and
-//   // used by the build method of the State. Fields in a Widget subclass are
-//   // always marked "final".
-//
-//   final String title;
-//
-//   @override
-//   State<MyMenuPage> createState() => _MyMenuPageState();
-// }
-//
-// class _MyMenuPageState extends State<MyMenuPage> {
-//   int _counter = 0;
-//
-//   void _incrementCounter() {
-//     setState(() {
-//       // This call to setState tells the Flutter framework that something has
-//       // changed in this State, which causes it to rerun the build method below
-//       // so that the display can reflect the updated values. If we changed
-//       // _counter without calling setState(), then the build method would not be
-//       // called again, and so nothing would appear to happen.
-//       _counter++;
-//     });
-//   }
+// class MenuPage extends StatelessWidget {
 //
 //   @override
 //   Widget build(BuildContext context) {
-//     // This method is rerun every time setState is called, for instance as done
-//     // by the _incrementCounter method above.
-//     //
-//     // The Flutter framework has been optimized to make rerunning build methods
-//     // fast, so that you can just rebuild anything that needs updating rather
-//     // than having to individually change instances of widgets.
 //     return Scaffold(
 //       appBar: AppBar(
-//         // Here we take the value from the MyMenuPage object that was created by
-//         // the App.build method, and use it to set our appbar title.
-//         title: Text(widget.title),
+//         title: Text('${LocalizationHelper.of(context)?.appName}'),
+//         actions: [
+//           PopupMenuButton<SupportedLocale>(
+//             itemBuilder: (context) {
+//               return SupportedLocale.values
+//                   .map<PopupMenuEntry<SupportedLocale>>((e) => PopupMenuItem(
+//                 child: Text('${e.name}'),
+//                 value: e,
+//               ))
+//                   .toList();
+//             },
+//             onSelected: (locale) {
+//               context.read(localeProvider.notifier).changeLanguage(locale);
+//             },
+//           )
+//         ],
 //       ),
-//       body: Center(
-//         // Center is a layout widget. It takes a single child and positions it
-//         // in the middle of the parent.
+//       body: Padding(
+//         padding: const EdgeInsets.all(16.0),
 //         child: Column(
-//           // Column is also a layout widget. It takes a list of children and
-//           // arranges them vertically. By default, it sizes itself to fit its
-//           // children horizontally, and tries to be as tall as its parent.
-//           //
-//           // Invoke "debug painting" (press "p" in the console, choose the
-//           // "Toggle Debug Paint" action from the Flutter Inspector in Android
-//           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-//           // to see the wireframe for each widget.
-//           //
-//           // Column has various properties to control how it sizes itself and
-//           // how it positions its children. Here we use mainAxisAlignment to
-//           // center the children vertically; the main axis here is the vertical
-//           // axis because Columns are vertical (the cross axis would be
-//           // horizontal).
-//           mainAxisAlignment: MainAxisAlignment.center,
+//           crossAxisAlignment: CrossAxisAlignment.start,
 //           children: <Widget>[
-//             const Text(
-//               'You have pushed the button this many times:',
+//             const Spacer(
+//               flex: 3,
 //             ),
-//             Text(
-//               '$_counter',
-//               style: Theme.of(context).textTheme.headlineMedium,
+//             const Spacer(),
+//             Text('${LocalizationHelper.of(context)?.description}',
+//                 textAlign: TextAlign.start,
+//                 style: Theme.of(context).textTheme.headline5),
+//             const Spacer(
+//               flex: 3,
 //             ),
 //           ],
 //         ),
 //       ),
-//       floatingActionButton: FloatingActionButton(
-//         onPressed: _incrementCounter,
-//         tooltip: 'Increment',
-//         child: const Icon(Icons.add),
-//       ), // This trailing comma makes auto-formatting nicer for build methods.
 //     );
 //   }
 // }
-
-// import 'package:easy_localization/easy_localization.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:fluttertoast/fluttertoast.dart';
-// import 'package:noise_meter/noise_meter.dart';
-// import 'package:flutter/material.dart';
-// import 'package:senseware/languageMainScreen.dart';
-// import 'package:senseware/languagepage.dart';
-// import 'dart:async';
-// import 'package:vibration/vibration.dart';
-// import 'package:permission_handler/permission_handler.dart';
-// //import 'package:senseware/login.dart';
-// import 'package:senseware/loginUI.dart';
-// //import 'package:senseware/signup.dart';
-// import 'package:senseware/SignupUI.dart';
-// import 'introduction_page.dart';
-// import 'welcomepage.dart';
-// import 'dart:math';
-// import 'languageMainScreen.dart';
-// //Color textcolor = Colors.deepOrange.shade800;
-//
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await EasyLocalization.ensureInitialized();
-//
-//   // runApp(EasyLocalization(
-//   //   supportedLocales: [
-//   //     Locale('en', 'US'),
-//   //     Locale('en', 'FR')
-//   //   ],
-//   //   path: 'assets/translations',
-//   //   saveLocale: true,
-//   //   fallbackLocale: Locale('en', 'US'),
-//   //   child: LanguageMainScreen(),
-//   // ));
-// //runApp(MyApp());
-//  // runApp(const MaterialApp(debugShowCheckedModeBanner: false,home : WelcomePage()));
 //
 //
+// // import 'package:flutter/material.dart';
+// //
+// // void main() {
+// //   runApp(const MyApp());
+// // }
+// //
+// // class MyApp extends StatelessWidget {
+// //   const MyApp({super.key});
+// //
+// //   // This widget is the root of your application.
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     return MaterialApp(
+// //       title: 'Flutter Demo',
+// //       theme: ThemeData(
+// //         // This is the theme of your application.
+// //         //
+// //         // Try running your application with "flutter run". You'll see the
+// //         // application has a blue toolbar. Then, without quitting the app, try
+// //         // changing the primarySwatch below to Colors.green and then invoke
+// //         // "hot reload" (press "r" in the console where you ran "flutter run",
+// //         // or simply save your changes to "hot reload" in a Flutter IDE).
+// //         // Notice that the counter didn't reset back to zero; the application
+// //         // is not restarted.
+// //         primarySwatch: Colors.blue,
+// //       ),
+// //       home: const MyMenuPage(title: 'Flutter Demo Home Page'),
+// //     );
+// //   }
+// // }
+// //
+// // class MyMenuPage extends StatefulWidget {
+// //   const MyMenuPage({super.key, required this.title});
+// //
+// //   // This widget is the home page of your application. It is stateful, meaning
+// //   // that it has a State object (defined below) that contains fields that affect
+// //   // how it looks.
+// //
+// //   // This class is the configuration for the state. It holds the values (in this
+// //   // case the title) provided by the parent (in this case the App widget) and
+// //   // used by the build method of the State. Fields in a Widget subclass are
+// //   // always marked "final".
+// //
+// //   final String title;
+// //
+// //   @override
+// //   State<MyMenuPage> createState() => _MyMenuPageState();
+// // }
+// //
+// // class _MyMenuPageState extends State<MyMenuPage> {
+// //   int _counter = 0;
+// //
+// //   void _incrementCounter() {
+// //     setState(() {
+// //       // This call to setState tells the Flutter framework that something has
+// //       // changed in this State, which causes it to rerun the build method below
+// //       // so that the display can reflect the updated values. If we changed
+// //       // _counter without calling setState(), then the build method would not be
+// //       // called again, and so nothing would appear to happen.
+// //       _counter++;
+// //     });
+// //   }
+// //
+// //   @override
+// //   Widget build(BuildContext context) {
+// //     // This method is rerun every time setState is called, for instance as done
+// //     // by the _incrementCounter method above.
+// //     //
+// //     // The Flutter framework has been optimized to make rerunning build methods
+// //     // fast, so that you can just rebuild anything that needs updating rather
+// //     // than having to individually change instances of widgets.
+// //     return Scaffold(
+// //       appBar: AppBar(
+// //         // Here we take the value from the MyMenuPage object that was created by
+// //         // the App.build method, and use it to set our appbar title.
+// //         title: Text(widget.title),
+// //       ),
+// //       body: Center(
+// //         // Center is a layout widget. It takes a single child and positions it
+// //         // in the middle of the parent.
+// //         child: Column(
+// //           // Column is also a layout widget. It takes a list of children and
+// //           // arranges them vertically. By default, it sizes itself to fit its
+// //           // children horizontally, and tries to be as tall as its parent.
+// //           //
+// //           // Invoke "debug painting" (press "p" in the console, choose the
+// //           // "Toggle Debug Paint" action from the Flutter Inspector in Android
+// //           // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+// //           // to see the wireframe for each widget.
+// //           //
+// //           // Column has various properties to control how it sizes itself and
+// //           // how it positions its children. Here we use mainAxisAlignment to
+// //           // center the children vertically; the main axis here is the vertical
+// //           // axis because Columns are vertical (the cross axis would be
+// //           // horizontal).
+// //           mainAxisAlignment: MainAxisAlignment.center,
+// //           children: <Widget>[
+// //             const Text(
+// //               'You have pushed the button this many times:',
+// //             ),
+// //             Text(
+// //               '$_counter',
+// //               style: Theme.of(context).textTheme.headlineMedium,
+// //             ),
+// //           ],
+// //         ),
+// //       ),
+// //       floatingActionButton: FloatingActionButton(
+// //         onPressed: _incrementCounter,
+// //         tooltip: 'Increment',
+// //         child: const Icon(Icons.add),
+// //       ), // This trailing comma makes auto-formatting nicer for build methods.
+// //     );
+// //   }
+// // }
 //
-// }
+// // import 'package:easy_localization/easy_localization.dart';
+// // import 'package:firebase_core/firebase_core.dart';
+// // import 'package:fluttertoast/fluttertoast.dart';
+// // import 'package:noise_meter/noise_meter.dart';
+// // import 'package:flutter/material.dart';
+// // import 'package:senseware/languageMainScreen.dart';
+// // import 'package:senseware/languagepage.dart';
+// // import 'dart:async';
+// // import 'package:vibration/vibration.dart';
+// // import 'package:permission_handler/permission_handler.dart';
+// // //import 'package:senseware/login.dart';
+// // import 'package:senseware/loginUI.dart';
+// // //import 'package:senseware/signup.dart';
+// // import 'package:senseware/SignupUI.dart';
+// // import 'introduction_page.dart';
+// // import 'welcomepage.dart';
+// // import 'dart:math';
+// // import 'languageMainScreen.dart';
+// // //Color textcolor = Colors.deepOrange.shade800;
+// //
+// // Future<void> main() async {
+// //   WidgetsFlutterBinding.ensureInitialized();
+// //   await EasyLocalization.ensureInitialized();
+// //
+// //   // runApp(EasyLocalization(
+// //   //   supportedLocales: [
+// //   //     Locale('en', 'US'),
+// //   //     Locale('en', 'FR')
+// //   //   ],
+// //   //   path: 'assets/translations',
+// //   //   saveLocale: true,
+// //   //   fallbackLocale: Locale('en', 'US'),
+// //   //   child: LanguageMainScreen(),
+// //   // ));
+// // //runApp(MyApp());
+// //  // runApp(const MaterialApp(debugShowCheckedModeBanner: false,home : WelcomePage()));
+// //
+// //
+// //
+// // }
